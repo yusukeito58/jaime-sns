d25dc986cfab58307041e40a71585185cd558fe7
Yusuke Ito <maroemon58@gmail.com>
2019-01-31 23:24:25 +0900

    Add login api

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,6 @@
 
 // User Regist
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+// User Login
+Route::post('/login', 'Auth\LoginController@login')->name('login');
---
+++ tests/Feature/LoginApiTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // create test user
+        $this->user = factory(User::class)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function authenticateAndReturnRegisteredUser()
+    {
+        $response = $this->json('POST', route('login'), [
+            'email' => $this->user->email,
+            'password' => 'secret'
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(['name' => $this->user->name]);
+
+        $this->assertAuthenticatedAs($this->user);
+    }
+}
