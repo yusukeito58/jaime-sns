6b56b1f1d608287d01b6f375c26b997198fcae91
Yusuke Ito <maroemon58@gmail.com>
2019-02-03 23:10:28 +0900

    Fix to keep login status

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,8 @@ Route::post('/login', 'Auth\LoginController@login')->name('login');
 
 // User Logout
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+// logged user
+Route::get('user', function() {
+    return Auth::user();
+})->name('user');
