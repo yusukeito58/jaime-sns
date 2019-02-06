64467829fc56181487151060f1bde70bd72d6e26
Yusuke Ito <maroemon58@gmail.com>
2019-02-06 23:17:21 +0900

    Fix element-ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
 import './bootstrap';
 
 import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 import router from './router';
 import store from './store';
