d79eda8703b889f22b4672cc5d7b54f1b9388e62
Yusuke Ito <maroemon58@gmail.com>
2019-02-09 00:14:18 +0900

    Add post table

---
+++ app/Post.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /** primary key */
+    protected $keyType = 'string';
+
+    /** ID length */
+    const ID_LENGTH = 12;
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        if (!array_get($this->attributes, 'id')) {
+            $this->setId();
+        }
+    }
+
+    private function setId()
+    {
+        $this->attributes['id'] = $this->getRandomId();
+    }
+
+    private function getRandomId()
+    {
+        $characters = array_merge(
+            range(0, 9), range('a', 'z'),
+            range('A', 'Z'), ['-', '_']
+        );
+
+        $length = count($characters);
+
+        $id = '';
+
+        for ($i = 0; $i < self::ID_LENGTH; $i++) {
+            $id .= $characters[random_int(0, $length - 1)];
+        }
+
+        return $id;
+    }
+}
