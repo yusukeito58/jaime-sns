9e71b710708a66b1947513c60b9238b0bec3dcc4
Yusuke Ito <maroemon58@gmail.com>
2019-04-05 07:09:58 +0900

    Add update update api #11

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+
+        $user->save();
+
+        Auth::setUser($user);
+
+        return response($user, 200);
+    }
+}
