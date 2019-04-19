2c52051a0e9af5796c32282bbf095443e2920a49
Yusuke Ito <maroemon58@gmail.com>
2019-04-19 16:04:45 +0900

    Remove unnecessary process #51

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,23 +1,14 @@
 <template>
-  <div>
-    <div v-if="isLogin">
-      <div :span="6">
+  <div class="container">
+    <div>
+      <div>
         <Profile/>
       </div>
-      <div :span="18">
+      <div>
         <button v-if="!showForm" @click="showForm = !showForm">Submit a post</button>
         <PostForm v-model="showForm"/>
         <PostList/>
       </div>
-    </div>
-    <div v-else>
-      <h1>Top Page</h1>
-      <router-link :to="{ path: '/login'}">
-        <button type="primary" plain>Login</button>
-      </router-link>
-      <router-link :to="{ path: '/regist' }">
-        <button>Regist</button>
-      </router-link>
     </div>
   </div>
 </template>
