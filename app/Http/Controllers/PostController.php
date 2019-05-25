63fb6e97c869796e230914708e8c110b959360bd
Yusuke Ito <maroemon58@gmail.com>
2019-05-25 20:48:57 +0900

    Add photo form at post form #63

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
+    const PHOTO_LENGTH = 12;
+
     public function __construct()
     {
         $this->middleware('auth');
@@ -26,13 +28,13 @@ class PostController extends Controller
         $post = new Post();
         $post->content = $request->content;
 
+        logger($request);
+        if (!empty($request->photo)) {
+            $extension = $request->photo->extension();
 
-        if (!empty($data['photo'])) {
-            $extension = $data['photo']->extension();
+            $photo_filename = $this->getRandomId() . '.' . $extension;
 
-            $photo_filename = $this->RandomId() . '.' . $extension;
-
-            Storage::cloud()->putFileAs('posts', $data['photo'], $photo_filename);
+            Storage::cloud()->putFileAs('/posts/', $request->photo, $photo_filename);
             $post->photo_filename = $photo_filename; 
         }
 
@@ -50,5 +52,27 @@ class PostController extends Controller
             ->orderBy(Post::CREATED_AT, 'desc')->paginate(10);
 
         return $posts;
+    }
+
+    /**
+     * ランダムなID値を生成する
+     * @return string
+     */
+    private function getRandomId()
+    {
+        $characters = array_merge(
+            range(0, 9), range('a', 'z'),
+            range('A', 'Z'), ['-', '_']
+        );
+
+        $length = count($characters);
+
+        $id = "";
+
+        for ($i = 0; $i < self::PHOTO_LENGTH; $i++) {
+            $id .= $characters[random_int(0, $length - 1)];
+        }
+
+        return $id;
     }
 }
